fd6ebc3ae86a4f56e62735fe070febb3c17770e1
Parv-gugnani <9765.parv@gmail.com>
2023-09-14 21:53:13 +0530

    icons fixed

--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -1,9 +1,9 @@
-import { GoTrashcan } from 'react-icons/go';
-import Button from './Button';
-import { removeUser } from '../store';
-import { useThunk } from '../hooks/use-thunk';
-import ExpandablePanel from './ExpandablePanel';
-import AlbumsList from './AlbumsList';
+import { FaTrash } from "react-icons/fa";
+import Button from "./Button";
+import { removeUser } from "../store";
+import { useThunk } from "../hooks/use-thunk";
+import ExpandablePanel from "./ExpandablePanel";
+import AlbumsList from "./AlbumsList";
 
 function UsersListItem({ user }) {
   const [doRemoveUser, isLoading, error] = useThunk(removeUser);
@@ -15,7 +15,7 @@ function UsersListItem({ user }) {
   const header = (
     <>
       <Button className="mr-3" loading={isLoading} onClick={handleClick}>
-        <GoTrashcan />
+        <FaTrash />
       </Button>
       {error && <div>Error deleting user.</div>}
       {user.name}
