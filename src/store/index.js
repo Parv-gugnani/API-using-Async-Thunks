22007bdbf5ebfdf3b02b63b3b1348d0443ba25bd
Parv-gugnani <9765.parv@gmail.com>
2023-09-16 15:57:49 +0530

    hook

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,24 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { usersReducer } from './slices/usersSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import { usersReducer } from "./slices/usersSlice";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { albumsApi } from "./apis/albumsApi";
+import { getDefaultNormalizer } from "@testing-library/react";
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
+    [albumsApi.reducerPath]: albumsApi.reducer,
+    // this is not array , this is another syntax , this does dont look for reducerpath property
+  },
+
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(albumsApi.middleware);
   },
 });
 
-export * from './thunks/fetchUsers';
-export * from './thunks/addUser';
-export * from './thunks/removeUser';
+setupListeners(store.dispatch);
+
+export * from "./thunks/fetchUsers";
+export * from "./thunks/addUser";
+export * from "./thunks/removeUser";
+export { useFetchAlbumsQuery } from "./apis/albumsApi";
