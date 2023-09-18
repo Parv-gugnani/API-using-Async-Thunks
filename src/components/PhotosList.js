a9ba6e7c3aa84a82a4eb253dab3ebef51ffe0c0a
Parv-gugnani <9765.parv@gmail.com>
2023-09-18 18:24:42 +0530

    REACT COURSE COMPLETED!

--- src/components/PhotosList.js
+++ src/components/PhotosList.js
@@ -1,14 +1,12 @@
 import { useFetchPhotosQuery, useAddPhotoMutation } from "../store";
 import Button from "./Button";
 import Skeleton from "./Skeleton";
-import PhotoListItem from "./PhotoListItem";
+import PhotosListItem from "./PhotoListItem";
 
 function PhotosList({ album }) {
   const { data, isFetching, error } = useFetchPhotosQuery(album);
-
   const [addPhoto, addPhotoResults] = useAddPhotoMutation();
 
-  //   event
   const handleAddPhoto = () => {
     addPhoto(album);
   };
@@ -17,22 +15,24 @@ function PhotosList({ album }) {
   if (isFetching) {
     content = <Skeleton className="h-8 w-8" times={4} />;
   } else if (error) {
-    content = <div>Error fetching photos</div>;
+    content = <div>Error fetching photos...</div>;
   } else {
     content = data.map((photo) => {
-      return <PhotoListItem key={photo.id} photo={photo} />;
+      return <PhotosListItem key={photo.id} photo={photo} />;
     });
   }
 
   return (
     <div>
-      <div className="m-2 flex-row items-center justify-between">
-        <h3 className="text-lg font-bold">Photos in {album.title}</h3>
-        <button loading={addPhotoResults.isLoading} onClick={handleAddPhoto}>
-          +Add photo
-        </button>
+      <div className="m-2 flex flex-row items-center justify-between">
+        <h3 className="text-lg font-bold">Photos In {album.title}</h3>
+        <Button loading={addPhotoResults.isLoading} onClick={handleAddPhoto}>
+          + Add Photo
+        </Button>
       </div>
-      <div>{content}</div>
+      <div className="mx-8 flex flex-row flex-wrap justify-center">
+        {content}
+      </div>
     </div>
   );
 }
